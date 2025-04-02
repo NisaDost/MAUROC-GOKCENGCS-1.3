9ef2b3b415d9b6293087cbf5abc2c2738932bed7
Nisa Dost <i.n.dost38@gmail.com>
2025-04-02 18:36:44 +0300

    ADD ros node

---
+++ GOKCEN-GCS/GCS.Core/ROSNode/Program.cs
@@ -0,0 +1,7 @@
+﻿using Rcl;
+using Rcl.Logging;
+
+await using var context = new RclContext(args);
+using var node = context.CreateNode("my_ros2_node");
+
+node.Logger.LogInformation($"Hello ROS 2 {RosEnvironment.Distribution}!");
